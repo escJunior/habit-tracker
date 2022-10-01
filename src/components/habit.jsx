c313ed9b58ae76bf2615c89b373c2955ee2520fe
escJunior <devescjunior@gmail.com>
2022-10-01 23:59:25 +0900

    Event Handling Part1

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+  handleIncremnet = () => {
+    this.props.onIncrement(this.props.habit.name);
+  };
+
+  handleDecremnet = () => {
+    this.props.onDecrement(this.props.habit.name);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit.name);
+  };
+
   render() {
+    const { name, count } = this.props.habit;
     return (
-      <>
-        <span className='habit-name'>Reading</span>
-        <span className='habit-name'>8</span>
-        <button>
+      <li className='habit'>
+        <span className='habit-name'>{name}</span>
+        <span className='habit-count'>{count}</span>
+        <button
+          className='habit-button habit-increase'
+          onClick={this.handleIncremnet}
+        >
           <i className='fa-solid fa-square-plus'></i>
         </button>
-        <button>
+        <button
+          className='habit-button habit-decrease'
+          onClick={this.handleDecremnet}
+        >
           <i className='fa-solid fa-square-minus'></i>
         </button>
-        <button>
+        <button
+          className='habit-button habit-delete'
+          onClick={this.handleDelete}
+        >
           <i className='fa-solid fa-trash'></i>
         </button>
-      </>
+      </li>
     );
   }
 }
